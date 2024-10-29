4e4c509284ad4b8267ac149b197b94153f346394
axat dubey <axatdubey@gmail.com>
2024-10-29 19:13:54 +0530

    resolving issue

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
-import User from "../models/user.model.js";
-import bcryptjs from "bcryptjs";
+import User from '../models/user.model.js';
+import bcryptjs from 'bcryptjs';
+import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
-
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -9,7 +9,7 @@ export const signup = async (req, res, next) => {
   const newUser = new User({ username, email, password: hashedPassword });
   try {
     await newUser.save();
-    res.status(201).json("User created successfully!");
+    res.status(201).json('User created successfully!');
   } catch (error) {
     next(error);
   }
@@ -31,7 +31,7 @@ export const signin = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}; 
+};
 
 export const google = async (req, res, next) => {
   try {
@@ -64,6 +64,15 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
   } catch (error) {
     next(error);
   }
